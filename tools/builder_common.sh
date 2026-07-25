f6160c6509e72475f4e2fc0a4fac29f20caf8911
KONTROL Tecnologia <guzzy@uol.com.br>
2026-07-25 15:26:36 -0300

    Make image packaging independent of ports keywords

--- tools/builder_common.sh
+++ tools/builder_common.sh
@@ -70,8 +70,12 @@ core_pkg_create_repo() {
 	ln -sf .latest/All ${CORE_PKG_ALL_PATH}
 	#ln -sf .latest/digests.txz ${CORE_PKG_PATH}/digests.txz
 	ln -sf .latest/meta.conf ${CORE_PKG_PATH}/meta.conf
-	ln -sf .latest/meta.txz ${CORE_PKG_PATH}/meta.txz
-	ln -sf .latest/packagesite.txz ${CORE_PKG_PATH}/packagesite.txz
+	for _ext in tzst txz; do
+		[ -f "${CORE_PKG_REAL_PATH}/meta.${_ext}" ] \
+			&& ln -sf ".latest/meta.${_ext}" "${CORE_PKG_PATH}/meta.${_ext}"
+		[ -f "${CORE_PKG_REAL_PATH}/packagesite.${_ext}" ] \
+			&& ln -sf ".latest/packagesite.${_ext}" "${CORE_PKG_PATH}/packagesite.${_ext}"
+	done
 }
 
 # Create core pkg (base, kernel)
@@ -102,6 +106,7 @@ core_pkg_create() {
 		-d "${CORE_PKG_REAL_PATH}/All" \
 		-a "${_abi}" \
 		-A "${_altabi}" \
+		-k "${BUILDER_TOOLS}/templates/pkg_keywords" \
 		|| print_error_pfS
 }
 
@@ -745,6 +750,14 @@ customize_stagearea_for_image() {
 			local _tgt_server="${PKG_REPO_SERVER_DEVEL}"
 		fi
 		for _db in ${FINAL_CHROOT_DIR}/var/db/pkg/repo-*sqlite; do
+			if [ ! -f "${_db}" ]; then
+				continue
+			fi
+			if ! /usr/local/bin/sqlite3 "${_db}" \
+				"select name from sqlite_master where type='table' and name='repodata'" \
+				| grep -q '^repodata$'; then
+				continue
+			fi
 			_cur=$(/usr/local/bin/sqlite3 ${_db} "${_read_cmd}")
 			_new=$(echo "${_cur}" | sed -e "s,^${PKG_REPO_SERVER_STAGING},${_tgt_server},")
 			/usr/local/bin/sqlite3 ${_db} "update repodata set value='${_new}' where key='packagesite'"
